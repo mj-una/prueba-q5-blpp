404041451b6bc2964bedd32e1254f93cae24839a
mj-una <133172570+mj-una@users.noreply.github.com>
2024-09-12 00:08:53 -0300

    v0.2

--- blipip/2-interfaz.js
+++ blipip/2-interfaz.js
@@ -6,60 +6,66 @@
 	// fill(200, 100, 190, 90);
 	// text("esc", 0, -420);
 
-	function mouseReleased() {
+	// function mouseReleased() {
 
-		// fuera del sketch
-		if (
-			 mouseX < 0 ||
-			mouseX > width ||
-			mouseY < 0 ||
-			mouseY > height
-		) return;
+	// 	// fuera del sketch
+	// 	if (
+	// 		 mouseX < 0 ||
+	// 		mouseX > width ||
+	// 		mouseY < 0 ||
+	// 		mouseY > height
+	// 	) return;
 	
-		// abrir fullscreen
-		if (flPortada) clickAbrirFs();
-		else clickCerrarFs();
-	}
+	// 	// abrir fullscreen
+	// 	if (flPortada) clickAbrirFs();
+	// 	else clickCerrarFs();
+	// }
 
-	class BotonEsc {
-		constructor(p_x, p_y, m_x, m_y) {
-			this.pos = {
-				x: p_x,
-				y: p_y
-			}
-			this.med = {
-				x: m_x,
-				y: m_y
-			}
-			this.txSz = 20;
-			this.yCorr = this.txSz * 0.05;
-		}
+	// class BotonEsc {
+	// 	constructor(p_x, p_y, m_x, m_y) {
+	// 		this.pos = {
+	// 			x: p_x,
+	// 			y: p_y
+	// 		}
+	// 		this.med = {
+	// 			x: m_x,
+	// 			y: m_y
+	// 		}
+	// 		this.txSz = 20;
+	// 		this.yCorr = this.txSz * 0.05;
+	// 	}
 
-		dibujar() {
-			push();
-			resetMatrix();
-			stroke(200, 100, 190, 60);
-			fill(200, 100, 190, 10);
-			rect(this.pos.x, this.pos.y, this.med.x, this.med.y, 6); // fondo
+	// 	dibujar() {
+	// 		push();
+	// 		resetMatrix();
+	// 		stroke(200, 100, 190, 60);
+	// 		fill(200, 100, 190, 10);
+	// 		rect(this.pos.x, this.pos.y, this.med.x, this.med.y, 6); // fondo
 
-			noStroke();
-			fill(200, 100, 190, 90)
-			textSize(this.txSz);
-			text("esc", this.pos.x, this.pos.y - this.yCorr) // texto
-			pop();
-		}
+	// 		noStroke();
+	// 		fill(200, 100, 190, 90)
+	// 		textSize(this.txSz);
+	// 		text("esc", this.pos.x, this.pos.y - this.yCorr) // texto
+	// 		pop();
+	// 	}
 
-		sobre(s_x, s_y) {
-			if (
-				s_x < this.pos.x + this.med.x &&
-				s_x < this.pos.x + this.med.x &&
-				s_y < this.pos.y + this.med.y
-			) return true;
-			return false;
-		}
+	// 	sobre(s_x, s_y) {
+	// 		if (
+	// 			s_x < this.pos.x + this.med.x &&
+	// 			s_x < this.pos.x + this.med.x &&
+	// 			s_y < this.pos.y + this.med.y
+	// 		) return true;
+	// 		return false;
+	// 	}
 
-		getX() { return this.pos.x; }
-		getY() { return this.pos.y; }
-		setX(s_x) { this.pos.x = s_x; }
-		setY(s_y) { this.pos.y = s_y; }
-	}
+	// 	getX() { return this.pos.x; }
+	// 	getY() { return this.pos.y; }
+	// 	setX(s_x) { this.pos.x = s_x; }
+	// 	setY(s_y) { this.pos.y = s_y; }
+	// }
+
+	document.addEventListener("DOMContentLoaded", function() {
+    // Asociamos la función al evento de click o touchstart
+    document.getElementById("q5Canvas0").addEventListener("click", clickAbrirFs);
+    document.getElementById("q5Canvas0").addEventListener("touchstart", clickAbrirFs);
+});
